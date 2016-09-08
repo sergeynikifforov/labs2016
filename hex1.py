9d65bb4d3a855c902ef8dbd5585cfc6b5c9ab766
sergeynikifforov <SergeyNikiforov761@gmail.com>
2016-09-08 17:28:24 +0300

    Add files via upload

---
+++ hex1.py
@@ -0,0 +1,4 @@
+import math
+
+x=int(input())
+print(math.log((1/math.e**(math.sin(x)+1)/(5/4+1/x**15)),1+x**2))
---
+++ hex5.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(-5, 5.01, 1)
+plt.xkcd()
+plt.plot(x,eval(input()))
+plt.grid(True)
+plt.show()
